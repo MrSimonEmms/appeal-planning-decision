719bd7b8b7a8c59a33c038d21e470c2eef62ce19
RichardDemeny <richarddemeny@gmail.com>
2021-02-01 16:44:47 +0000

    ac3a1

--- e2e-tests/cypress/integration/common/appellant-submission-check-your-answers.js
+++ e2e-tests/cypress/integration/common/appellant-submission-check-your-answers.js
@@ -221,15 +221,65 @@ When('the user confirms that they are happy with their answers', () => {
 });
 
 Given('changes are made for About you section', () => {
+  cy.goToTaskListPage();
 
+  cy.goToWhoAreYouPage();
+  cy.answerYesOriginalAppellant();
+  cy.clickSaveAndContinue();
+
+  cy.provideDetailsName('Valid Name');
+  cy.provideDetailsEmail('valid@email.com');
+  cy.clickSaveAndContinue();
+
+  cy.promptUserToProvidePlanningApplicationNumber();
+  cy.providePlanningApplicationNumber('ValidNumber/12345');
+  cy.goToPlanningApplicationSubmission();
+  cy.uploadPlanningApplicationFile('appeal-statement-valid.doc');
+  cy.clickSaveAndContinue();
+
+  cy.goToDecisionLetterPage();
+  cy.uploadDecisionLetterFile('appeal-statement-valid.doc');
+  cy.clickSaveAndContinue();
+
+  cy.goToAppealStatementSubmission();
+  cy.checkNoSensitiveInformation();
+  cy.uploadAppealStatementFile('appeal-statement-valid.doc');
+  cy.clickSaveAndContinue();
+
+  cy.goToSiteAddressPage();
+  cy.provideAddressLine1('1 Taylor Road');
+  cy.provideAddressLine2('Clifton');
+  cy.provideTownOrCity('Bristol');
+  cy.provideCounty('South Glos');
+  cy.providePostcode('BS8 1TG');
+  cy.clickSaveAndContinue();
+
+  cy.goToWholeSiteOwnerPage();
+  cy.answerOwnsTheWholeAppeal();
+  cy.clickSaveAndContinue();
+
+  cy.goToAccessSitePage();
+  cy.answerCanSeeTheWholeAppeal();
+  cy.clickSaveAndContinue();
+
+  cy.goToHealthAndSafetyPage();
+  cy.answerSiteHasNoIssues();
+  cy.clickSaveAndContinue();
+
+  cy.goToYourDetailsPage();
+  cy.provideDetailsName('New Valid Name');
+  cy.provideDetailsEmail('new-valid@email.com');
+  cy.clickSaveAndContinue();
 });
 
 When('Check Your Answers is presented', () => {
-
+  cy.clickCheckYourAnswers();
 });
 
 Then('the updated values for About you section are displayed', () => {
-
+  cy.confirmCheckYourAnswersPage();
+  cy.get('[data-cy="appellant-name"]').should('have.value', 'New Valid Name');
+  cy.get('[data-cy="appellant-email"]').should('have.value', 'new-valid@email.com');
 });
 
 Given('changes are made for About the original planning application section', () => {
